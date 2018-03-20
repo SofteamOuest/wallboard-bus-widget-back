12379ad23d9a2d528fe2972bf7c5b49b70f028bc
yvzn <mail@ludeo.net>
2018-03-20 14:00:48 +0100

    Renaming

--- tests/test_theoretical_schedule.py
+++ tests/test_theoretical_schedule.py
@@ -7,11 +7,11 @@ class TestTheoreticalScheduleResource(unittest.TestCase):
     def setUp(self):
         self.app = bus_api.app.test_client()
 
-    def test_get_with_no_parameters(self):
+    def test_get_with_no_parameter_should_return_error(self):
         response = self.app.get('/theoretical')
         self.assertTrue('Missing required parameter' in response.get_data().decode(sys.getdefaultencoding()))
 
-    def test_get_with_parameters(self):
+    def test_get_with_parameters_should_not_return_error(self):
         response = self.app.get('/theoretical?stop=Aline=1&line&2')
         self.assertFalse('Missing required parameter' in response.get_data().decode(sys.getdefaultencoding()))
 
